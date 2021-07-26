854f264b4e87f525ab7bbd7ad536ec6f0f377343
1 <tawers6103@gmail.com>
2021-07-26 16:17:11 +0300

    js

--- main.js
+++ main.js
@@ -2,5 +2,30 @@
 //Отримати відповідь з цього ресурсу відповідь, та вивести в документ як в прикладі на занятті
 //https://jsonplaceholder.typicode.com/posts
 
+fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(value => value.json())
+    .then(posts => {
+
+        for (const post of posts) {
+            let userDiv = document.createElement('div');
+            userDiv.innerText = `${post.userId} - ${post.id} - ${post.title} -${post.body} `;
+            let target = document.getElementById('target');
+            target.append(userDiv);
+        }
+
+    });
+//2.
  ////Отримати відповідь з цього ресурсу відповідь, та вивести в документ як в прикладі на занятті
-//shttps://jsonplaceholder.typicode.com/comments
+//https://jsonplaceholder.typicode.com/comments
+fetch('https://jsonplaceholder.typicode.com/comments')
+    .then(value => value.json())
+    .then(comments => {
+
+        for (const comment of comments) {
+            let userDiv = document.createElement('div');
+            userDiv.innerText = `${comment.postId} - ${comment.id} - ${comment.name} - ${comment.email} - ${comment.body}`;
+            let target = document.getElementById('target');
+            target.append(userDiv);
+        }
+
+    });
